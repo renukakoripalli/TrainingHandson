69f9106dcd1989cd7b9cc968a7fe0be9f6fd5626
renukakoripalli <koripallirenuka03@gmail.com>
2020-07-16 18:17:01 +0530

    third commit

---
+++ 15-07-2020/HandsOnDelegate/HandsOnDelegate/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace HandsOnDelegate
+{
+    public delegate void Caluculate(int width, int height);
+    class Program
+    {
+        public static void AreaAndPerimeter(int width, int height)
+        {
+            Console.WriteLine("Area: {0}", width * height);
+            Console.WriteLine("Perimeter: {0}", 2 * width + 2 * height);
+        }
+        static void Main()
+
+        {
+            Caluculate d = new Caluculate(AreaAndPerimeter);
+            d(2, 3);
+        }
+
+    }
+
+}
