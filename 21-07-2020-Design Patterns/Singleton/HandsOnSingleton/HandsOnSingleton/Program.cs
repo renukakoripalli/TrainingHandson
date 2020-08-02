82371c51ac2f9c33be93f28408d2b4ee41efc320
renukakoripalli <koripallirenuka03@gmail.com>
2020-08-02 23:04:46 +0530

    fifth commit

---
+++ 21-07-2020-Design Patterns/Singleton/HandsOnSingleton/HandsOnSingleton/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HandsOnSingleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Singleton a=Singleton.GetInstance;
+            //a.Area(2,3);
+            //Console.WriteLine(a);
+            //Singleton b = Singleton.GetInstance;
+            //b.Perimeter(2, 3);
+            //Console.WriteLine(b);
+           Console.WriteLine("Area of rectangle : " + Singleton.GetInstance.Area(2,2));
+           Console.WriteLine("Perimeter of rectangle : " + Singleton.GetInstance.Perimeter(3, 2));
+           Console.ReadLine();
+        }
+    }
+}
---
+++ 21-07-2020-Design Patterns/Singleton/HandsOnSingleton/HandsOnSingleton/Singleton.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HandsOnSingleton
+{
+    public sealed class Singleton
+    {
+        private static int counter = 0;
+        private static Singleton instance = null;
+        public static Singleton GetInstance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new Singleton();
+                return instance;
+            }
+        }
+
+        private Singleton()
+        {
+            counter++;
+            Console.WriteLine("Counter Value " + counter.ToString());
+        }
+
+        //public double width { get; set; }
+        //public double height { get; set; }
+        public double Area(double width,double height)
+        {
+            return width*height;
+        }
+        public double Perimeter(double width,double height)
+        {
+            return 2*width+2*height;
+        }
+        
+    }
+}
+
